f32b876d8dcaf160f958259cb5483f31e01b23c9
Sidhu <codingsidhu@gmail.com>
2022-12-21 21:04:05 +0530

    day 1

---
+++ variable and containers.php
@@ -0,0 +1,13 @@
+<?php
+    // variables are containers for storing information
+    // starts with a $
+
+    $name = "Siddharth";
+    $Roll_No = 19;
+
+    echo "The name of the student is $name and his roll no is $Roll_No. <br>";
+
+    echo "$name is an sowfetwar enginer";
+
+
+?>
