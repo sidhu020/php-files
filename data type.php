7da9301df63482402c980235d3df0a81e81d0b1b
Sidhu <codingsidhu@gmail.com>
2022-12-22 16:30:30 +0530

    day 2

---
+++ data type.php
@@ -0,0 +1,70 @@
+<?php
+    $name = "siddharth";
+    $income = 12000;
+
+    /*
+    >php data type
+
+    1. String
+    2. integer
+    3. float
+    4. boolean
+    5. array
+    6. object
+    7. null
+    */
+
+    // string data type example
+
+    $name = "siddharth";
+    $frnd = "katan";
+    echo "$name is frnd of $frnd<br>";
+
+    // Integer - non decimal number
+
+    echo "<br>";
+    $income = 1000;
+    echo "<br>";
+    $mark = 95;
+
+    echo "<br>";
+    echo $income;
+    echo "<br>";
+    echo $mark;
+
+    // Float - Decimal Number
+    echo "<br>";
+    $income = 1000.5;
+    echo "<br>";
+    $mark = 95.5;
+
+    echo $income;
+    echo $mark;
+
+    // Boolean - can be either True or fulse
+    echo "<br>";
+    $frnd = true;
+    echo "<br>";
+    $mark = 95;
+
+    echo $frnd;
+
+
+    $name="siddharth";
+    $mark=95;
+    $true=true;
+    $false=false;
+    $pr=98.58;
+   // $null= ;
+
+    echo var_dump ($name);
+    echo var_dump($mark);
+    echo var_dump($true);
+    echo var_dump($false);
+    echo var_dump($pr);
+
+    $friends = array("karan","girish","drov");
+
+
+
+?>
