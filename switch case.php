f03676e7dc018b71e5153a95194179f8f48e1ffd
Sidhu <codingsidhu@gmail.com>
2023-01-09 11:53:17 +0530

    switch case in php

---
+++ switch case.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $ch = 'u';
+
+        switch($ch)
+        {
+            case $ch == 'A' || $ch == 'a':
+                echo "Given Charechter is Voval A";
+                break;
+
+
+                case $ch == 'E' || $ch == 'e':
+                    echo "Given Charechter is Voval E";
+                    break;
+
+                    case $ch == 'I' || $ch == 'i':
+                        echo "Given Charechter is Voval I";
+                        break;
+
+                        case $ch == 'O' || $ch == 'o':
+                            echo "Given Charechter is Voval O";
+                            break;
+
+
+                            case $ch == 'U' || $ch == 'u':
+                                echo "Given Charechter is Voval U";
+                                break;
+
+
+        }
+
+
+
+
+    ?>
+
+</body>
+</html>
