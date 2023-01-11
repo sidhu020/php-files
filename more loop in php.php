6312c7ab841cc2644364f1ea5e06feec4f978b7f
Sidhu <codingsidhu@gmail.com>
2023-01-11 12:44:12 +0530

    Q1 comp. 11/01/2023 12:44pm

---
+++ more loop in php.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+
+
+for($a=1;$a<=5;$a++)
+{
+    for($b=1;$b<=$a;$b++)
+    {
+        echo"$a";
+    }
+
+        echo "<br>";
+}
+for($a=5;$a>=1;$a--)
+{
+    for($b=$a;$b>=1;$b--)
+    {
+        echo"$a";
+    }
+
+        echo "<br>";
+}
+
+?>
+
+</body>
+</html>
